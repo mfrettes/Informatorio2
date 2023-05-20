045ea1ab0c30156b8e703570f84ecab347b3d6b6
Mario FRETES <129708122+mfrettes@users.noreply.github.com>
2023-05-19 22:16:00 -0300

    Update desafio4-grupo18.py
    Actualizado por el grupo 18 conformado por Mónica Inés Cesana Bernasconi, Nancy Cecilia Ojeda,
    Juan Kiel Sodja Bogado, Emanuel Domingo Montenegro y Mario Luis	Fretes

--- desafio4-grupo18.py
+++ desafio4-grupo18.py
@@ -1,69 +1,237 @@
-#otro ejemplo, usando funciones, del desafío 4: "La Inmobiliaria"
-#Lista de inmuebles
-inmuebles = [{'año': 2010, 'metros': 150, 'habitaciones': 4, 'garaje': True, 'zona': 'C', 'estado': 'Disponible'},
-             {'año': 2016, 'metros': 80, 'aciones': 2, 'garaje': False, 'zona': 'B', 'estado': 'Reservado'},
-             {'año': 2000, 'metros': 180, 'habitaciones': 4, 'garaje': True, 'zona': 'A', 'estado': 'Disponible'},
-             {'año': 2015, 'metros': 95, 'habitaciones': 3, 'garaje': True, 'zona': 'B', 'estado': 'Vendido'},
-             {'año': 2008, 'metros': 60, 'habitaciones': 2, 'garaje': False, 'zona': 'C', 'estado': 'Disponible'}]
+'''
+Realizado en grupo por Mónica Inés Cesana Bernasconi, Nancy Cecilia Ojeda,
+Juan Kiel Sodja Bogado, Emanuel Domingo Montenegro y Mario Luis	Fretes.
 
-# Función para agregar un nuevo inmueble a la lista
-def agregar_inmueble(inmuebles, nuevo_inmueble):
-    if validar_inmueble(nuevo_inmueble): # Verificar que el inmueble cumple las reglas de validación
-        inmuebles.append(nuevo_inmueble)
-        print("Inmueble agregado con éxito")
+https://github.com/mfrettes/Informatorio2/edit/main/desafio4-grupo18.py
+'''
+def agregar_inmueble(lista_inmuebles):
+    nuevo_inmueble = {}
+    nuevo_inmueble['año'] = int(input("Año: "))
+    nuevo_inmueble['metros'] = int(input("Metros cuadrados: "))
+    nuevo_inmueble['habitaciones'] = int(input("Cantidad de habitaciones: "))
+    nuevo_inmueble['garaje'] = input("¿Tiene garaje? (S/N): ").upper() == "S"
+    nuevo_inmueble['zona'] = input("Zona (A, B, C): ").upper()
+    nuevo_inmueble['estado'] = input("Estado (Disponible, Reservado, Vendido): ").capitalize()
+
+    if validar_inmueble(nuevo_inmueble):
+        lista_inmuebles.append(nuevo_inmueble)
+        guardar_inmuebles(lista_inmuebles)
+        print("Inmueble agregado correctamente.")
     else:
-        print("El inmueble no cumple las reglas de validación")
+        print("Error: El inmueble no cumple los requisitos mínimos.")
 
-# Función para editar un inmueble existente en la lista
-def editar_inmueble(inmuebles, indice, nuevo_inmueble):
-    if validar_inmueble(nuevo_inmueble): # Verificar que el inmueble cumple las reglas de validación
-        inmuebles[indice] = nuevo_inmueble
-        print("Inmueble editado con éxito")
+
+def editar_inmueble(lista_inmuebles):
+    mostrar_inmuebles(lista_inmuebles)
+    index = int(input("Ingrese el número del inmueble que desea editar: "))
+    if index >= 0 and index < len(lista_inmuebles):
+        inmueble = lista_inmuebles[index]
+
+        while True:
+            print("\nMenú de corrección:")
+            print("1. Corregir año")
+            print("2. Corregir metros")
+            print("3. Corregir habitaciones")
+            print("4. Corregir garaje")
+            print("5. Corregir zona")
+            print("6. Volver al menú principal")
+
+            opcion_correccion = input("Ingrese su opción: ")
+            if opcion_correccion == "1":
+                nuevo_año = int(input("Ingrese el nuevo año: "))
+                if nuevo_año >= 2000:
+                    inmueble['año'] = nuevo_año
+                    guardar_inmuebles(lista_inmuebles)
+                    print("Año corregido correctamente.")
+                else:
+                    print("Error: El año debe ser igual o mayor a 2000.")
+            elif opcion_correccion == "2":
+                nuevo_metros = int(input("Ingrese los nuevos metros cuadrados: "))
+                if nuevo_metros >= 60:
+                    inmueble['metros'] = nuevo_metros
+                    guardar_inmuebles(lista_inmuebles)
+                    print("Metros cuadrados corregidos correctamente.")
+                else:
+                    print("Error: Los metros cuadrados deben ser igual o mayores a 60.")
+            elif opcion_correccion == "3":
+                nuevo_habitaciones = int(input("Ingrese la nueva cantidad de habitaciones: "))
+                if nuevo_habitaciones >= 2:
+                    inmueble['habitaciones'] = nuevo_habitaciones
+                    guardar_inmuebles(lista_inmuebles)
+                    print("Cantidad de habitaciones corregida correctamente.")
+                else:
+                    print("Error: La cantidad de habitaciones debe ser igual o mayor a 2.")
+            elif opcion_correccion == "4":
+                nuevo_garaje = input("¿Tiene garaje? (S/N): ").upper()
+                if nuevo_garaje in ['S', 'N']:
+                    inmueble['garaje'] = nuevo_garaje == "S"
+                    guardar_inmuebles(lista_inmuebles)
+                    print("Garaje corregido correctamente.")
+                else:
+                    print("Error: Opción inválida. Debe ingresar S para sí o N para no.")
+            elif opcion_correccion == "5":
+                nuevo_zona = input("Ingrese la nueva zona (A, B, C): ").upper()
+                if nuevo_zona in ['A', 'B', 'C']:
+                    inmueble['zona'] = nuevo_zona
+                    guardar_inmuebles(lista_inmuebles)
+                    print("Zona corregida correctamente.")
+                else:
+                    print("Error: Zona inválida. Debe ingresar A, B o C.")
+            elif opcion_correccion == "6":
+                print("Volviendo al menú principal.")
+                break
+            else:
+                print("Opción inválida. Por favor, seleccione una opción válida del menú.")
+
     else:
-        print("El inmueble no cumple las reglas de validación")
+        print("Error: Número de inmueble inválido.")
 
-# Función para eliminar un inmueble existente de la lista
-def eliminar_inmueble(inmuebles, indice):
-    del inmuebles[indice]
-    print("Inmueble eliminado con éxito")
 
-# Función para cambiar el estado de un inmueble existente en la lista
-def cambiar_estado(inmuebles, indice, nuevo_estado):
-    inmuebles[indice]['estado'] = nuevo_estado
-    print("Estado de inmueble modificado con éxito")
+def eliminar_inmueble(lista_inmuebles):
+    mostrar_inmuebles(lista_inmuebles)
+    index = int(input("Ingrese el número del inmueble que desea eliminar: "))
+    if index >= 0 and index < len(lista_inmuebles):
+        inmueble = lista_inmuebles.pop(index)
+        guardar_inmuebles(lista_inmuebles)
+        print("Inmueble eliminado correctamente.")
+    else:
+        print("Error: Número de inmueble inválido.")
 
-# Función para buscar inmuebles según un presupuesto dado
-def buscar_inmuebles(inmuebles, presupuesto):
-    inmuebles_encontrados = []
-    for inmueble in inmuebles:
-        if inmueble['estado'] in ['Disponible', 'Reservado'] and inmueble['año'] >= 2000 and inmueble['metros'] >= 60 and inmueble['habitaciones'] >= 2 and inmueble['zona'] in ['A', 'B', 'C']:
-            precio = calcular_precio(inmueble)
-            if precio <= presupuesto:
-                inmueble_con_precio = inmueble.copy()
-                inmueble_con_precio['precio'] = precio
-                inmuebles_encontrados.append(inmueble_con_precio)
-    return inmuebles_encontrados
 
-# Función para validar si un inmueble cumple las reglas de validación
 def validar_inmueble(inmueble):
-    if inmueble['zona'] not in ['A', 'B', 'C']:
-        return False
-    if inmueble['estado'] not in ['Disponible', 'Reservado', 'Vendido']:
-        return False
     if inmueble['año'] < 2000:
         return False
     if inmueble['metros'] < 60:
         return False
-    if inmueble['habitaciones'] <2:
+    if inmueble['habitaciones'] < 2:
+        return False
+    if inmueble['zona'] not in ['A', 'B', 'C']:
+        return False
+    if inmueble['estado'] not in ['Disponible', 'Reservado', 'Vendido']:
         return False
     return True
 
-# Función para calcular el precio de un inmueble según la zona
+
 def calcular_precio(inmueble):
-    if inmueble['zona'] == 'A':
-        precio = (inmueble['metros'] * 100 + inmueble['habitaciones'] * 500 + inmueble['garaje'] * 1500) * (1 - (2021 - inmueble['año']) / 100)
-    elif inmueble['zona'] == 'B':
-        precio = (inmueble['metros'] * 100 + inmueble['habitaciones'] * 500 + inmueble['garaje'] * 1000) * (1 - (2021 - inmueble['año']) / 100)
+    antiguedad = 2023 - inmueble['año']
+    metros = inmueble['metros']
+    habitaciones = inmueble['habitaciones']
+    garaje = inmueble['garaje']
+    zona = inmueble['zona']
+
+    if zona == 'A':
+        precio = (metros * 100 + habitaciones * 500 + garaje * 1500) * (1 - antiguedad / 100)
+    elif zona == 'B':
+        precio = (metros * 100 + habitaciones * 500 + garaje * 1500) * (1 - antiguedad / 100) * 1.5
     else:
-        precio = (inmueble['metros'] * 100 + inmueble['habitaciones'] * 500 + inmueble['garaje'] * 500) * (1 - (2021 - inmueble['año']) / 100)
-    return precio                                                                                                                       
+        precio = (metros * 100 + habitaciones * 500 + garaje * 1500) * (1 - antiguedad / 100) * 2
+
+    return precio
+
+
+def buscar_inmuebles_por_presupuesto(lista_inmuebles):
+    presupuesto = float(input("Ingrese su presupuesto: "))
+
+    inmuebles_encontrados = []
+    for inmueble in lista_inmuebles:
+        if inmueble['estado'] in ['Disponible', 'Reservado']:
+            precio = calcular_precio(inmueble)
+            if precio <= presupuesto:
+                inmueble['precio'] = precio
+                inmuebles_encontrados.append(inmueble)
+
+    return inmuebles_encontrados
+
+
+def mostrar_inmuebles(lista_inmuebles):
+    for i, inmueble in enumerate(lista_inmuebles):
+        print(f"Inmueble {i}:")
+        print(f"Año: {inmueble['año']}")
+        print(f"Metros cuadrados: {inmueble['metros']}")
+        print(f"Habitaciones: {inmueble['habitaciones']}")
+        print(f"Garaje: {'Sí' if inmueble['garaje'] else 'No'}")
+        print(f"Zona: {inmueble['zona']}")
+        print(f"Estado: {inmueble['estado']}")
+        if 'precio' in inmueble:
+            print(f"Precio: {inmueble['precio']}")
+        print()
+
+
+def guardar_inmuebles(lista_inmuebles):
+    with open('inmuebles.txt', 'w') as archivo:
+        for inmueble in lista_inmuebles:
+            archivo.write(f"{inmueble}\n")
+
+
+def cargar_inmuebles():
+    try:
+        with open('inmuebles.txt', 'r') as archivo:
+            lineas = archivo.readlines()
+            lista_inmuebles = [eval(linea) for linea in lineas]
+            return lista_inmuebles
+    except FileNotFoundError:
+        return []
+
+
+def mostrar_menu():
+    print("\n***************Bienvenido a la Inmobiliaria XYZ")
+    print('\n')
+    print("1. Agregar, editar o eliminar inmuebles")
+    print("2. Cambiar el estado de un inmueble")
+    print("3. Búsqueda de inmuebles por presupuesto")
+    print("4. Guardar y salir")
+
+
+def modificar_estado_inmueble(lista_inmuebles):
+    mostrar_inmuebles(lista_inmuebles)
+    index = int(input("Ingrese el número del inmueble que desea modificar: "))
+    if index >= 0 and index < len(lista_inmuebles):
+        inmueble = lista_inmuebles[index]
+
+        nuevo_estado = input(f"Ingrese el nuevo estado del inmueble (Disponible, Reservado, Vendido): ").capitalize()
+        if nuevo_estado in ['Disponible', 'Reservado', 'Vendido']:
+            inmueble['estado'] = nuevo_estado
+            guardar_inmuebles(lista_inmuebles)
+            print("Estado del inmueble modificado correctamente.")
+        else:
+            print("Error: Estado inválido. Debe ingresar Disponible, Reservado o Vendido.")
+    else:
+        print("Error: Número de inmueble inválido.")
+
+
+lista_inmuebles = cargar_inmuebles()
+
+while True:
+    mostrar_menu()
+    opcion = input("\nIngrese su opción: ")
+
+    if opcion == "1":
+        print("1. Agregar inmueble")
+        print("2. Editar inmueble")
+        print("3. Eliminar inmueble")
+
+        subopcion = input("Ingrese su opción: ")
+
+        if subopcion == "1":
+            agregar_inmueble(lista_inmuebles)
+        elif subopcion == "2":
+            editar_inmueble(lista_inmuebles)
+        elif subopcion == "3":
+            eliminar_inmueble(lista_inmuebles)
+        else:
+            print("\nOpción inválida.")
+
+    elif opcion == "2":
+        modificar_estado_inmueble(lista_inmuebles)
+
+    elif opcion == "3":
+        inmuebles_encontrados = buscar_inmuebles_por_presupuesto(lista_inmuebles)
+        print("\nInmuebles encontrados:")
+        mostrar_inmuebles(inmuebles_encontrados)
+
+    elif opcion == "4":
+        print("\n¡Gracias por usar nuestro sistema!")
+        break
+
+    else:
+        print("\nOpción inválida.")
